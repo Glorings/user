d1816005d4bb4b8cf9ad33b02303fae53ea55b01
Glorings <18668465735@163.com>
2019-07-09 23:49:18 +0800

    turbine集群监控添加

--- src/main/java/com/example/user/UserApplication.java
+++ src/main/java/com/example/user/UserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@SpringBootApplication
@@ -15,6 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //@EnableCircuitBreaker
 @EnableHystrixDashboard
 @SpringCloudApplication
+@EnableTurbine
 public class UserApplication {
 
     public static void main(String[] args) {
